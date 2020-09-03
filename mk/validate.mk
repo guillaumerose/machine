ee1027b1dbd2044f0dc0cb4e896c72bc5904fcab
Guillaume Rose <gurose@redhat.com>
2020-09-03 10:18:15 +0200

    Use golangci-lint instead of golint

--- mk/validate.mk
+++ mk/validate.mk
@@ -15,6 +15,4 @@ vet:
 	@test -z "$$(go vet $(PKGS) 2>&1 | tee /dev/stderr)"
 
 lint:
-	$(if $(GOLINT), , \
-		$(error Please install golint: go get -u golang.org/x/lint/golint))
-	@test -z "$$($(GOLINT) ./... 2>&1 | grep -v vendor/ | grep -v "cli/" | grep -v "amazonec2/" |grep -v "openstack/" |grep -v "softlayer/" | grep -v "should have comment" | tee /dev/stderr)"
+	golangci-lint run $(PKGS)
